fd8f9c4b176277529b5a82bbbe9c179b90f6b2dc
Milan <MiVents@users.noreply.github.com>
2020-08-15 17:00:27 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,3 @@
-cd ~/screenmi-client && git pull && electron --no-sandbox . 
+#!/bin/bash
+cd ~/screenmi-client && source ~/.nvm/nvm.sh && git pull && electron --no-sandbox . 
+read -p "Press enter to continue"
